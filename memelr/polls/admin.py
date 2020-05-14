53b397199a5c33997da19d491ceaf3fabb6d00aa
mike <mike@mikedbolton.com>
2020-05-13 22:20:42 -0500

    Updating tutorial progress: - posts and comment classes are created - can post images from admin portal - strict typing preserved - db migrations performed - superuser and two test accounts created - settings updated

--- memelr/polls/admin.py
+++ memelr/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Response
+
+admin.site.register(Post)
+admin.site.register(Response)
--- memelr/polls/models.py
+++ memelr/polls/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+import django
 # Create your models here.
 class Post(models.Model):
+    post_title = models.CharField(max_length=255, default="post title goes here numbnuts")
     post_content = models.ImageField()
-    post_date = models.DateTimeField('date posted')
+    post_date = models.DateTimeField('date posted', default=django.utils.timezone.now())
     postvotes = models.IntegerField(default=0)
 
 class Response(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     respvotes = models.IntegerField(default=0)
-    submission_time = models.DateTimeField('date responded')
+    submission_time = models.DateTimeField('date responded', default=django.utils.timezone.now())
